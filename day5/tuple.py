ce58a1635fe36b71153b3c85a1a87b324dcd43fb
ZaynMohd <zainulabedeen923@gmail.com>
2026-07-26 20:03:47 +0530

    Complete Day 5 of Pyhton learning journey

---
+++ day5/tuple.py
@@ -0,0 +1,23 @@
+#Dictionary
+# student = {"name": "Zain",
+#            "age": 21,
+#            "course": "Data Science",
+#            "goal": "AI Engineer",
+#            "city": "Hyderabad"
+
+#            }
+# print(student["name"])
+# print(student["age"])
+# print(student["course"])
+# print(student["goal"])
+
+#Tuples
+# print(student["city"])
+# cities = ("Hyderabad","Delhi","Mumbai")
+# print(cities[1])
+# print(cities[-1])
+# cities[0] = "Chennai" #we can't add any values to the tuple
+
+#Sets
+colors = {"Red","Blue","Red","Orange"}
+print(colors)
