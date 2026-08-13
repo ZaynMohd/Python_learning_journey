cc9e1a9584cc6bf81103db3473a24793a31d30ed
ZaynMohd <zainulabedeen923@gmail.com>
2026-08-13 13:10:46 +0530

    Complete Day 22- Numpy Adv1

---
+++ day22/numpy_adv.py
@@ -0,0 +1,70 @@
+# arr[row, column]
+
+#np.zeros()
+# import numpy as np
+# arr = np.zeros((3,4))
+# print(arr)
+
+#np.ones()
+# import numpy as za
+# arr = za.ones((2,2))
+# print(arr)
+
+#np.arange()
+# import numpy as za
+# arc = za.arange(1,11)
+# aaa = za.arange(2,20,3)   #(start,stop,step)
+# print(arc)
+# print(aaa)
+
+# import numpy as np
+# arr = np.linspace(0,10,6)  #(start,stop,total_values)
+# print(arr)
+
+# import numpy as np
+# arr = np.array([1,2,3,4,5,6,7,8,9,10])
+# print(arr.min())
+# print(arr.max())
+
+# #sum of array
+# print(arr.sum())
+
+# import numpy as np
+# arr = np.array([
+#     [1,2,3],
+#     [4,5,6]
+# ])
+# print(arr.sum(axis=0))
+
+# import numpy as np
+# arr = np.array([
+#     [1,2,3],
+#     [4,5,6]
+# ])
+# print(arr.sum(axis=0))
+# print(arr.sum(axis=1))
+
+# import numpy as np
+# arr = np.array([
+#     [10,20,30],
+#     [40,50,60]
+# ])
+# print(arr.mean(axis=1))
+# print(arr.max())
+
+# import numpy as np
+# arr = np.array([
+#     [5,10,15],
+#     [20,25,30]
+# ])
+# print(arr.shape)
+# print(arr.sum())
+# print(arr.max())
+# print(arr.mean())
+
+import numpy as np
+arr = np.array([
+    [10,20,30],
+    [40,50,60]
+])
+print(arr.max())
